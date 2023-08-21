1727d6e8f2e1be92f78a6d7d333325d0ae1dc2bd
BabaDorin <bvd.dorin@gmail.com>
2023-08-21 20:12:45 +0300

    project init + add models

---
+++ DealershipManager/DealershipManager/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace DealershipManager.Models
+{
+    public enum Category
+    {
+        SUV,
+        Hatchback,
+        Universal,
+        Crossover,
+        Sedan
+    }
+}
---
+++ DealershipManager/DealershipManager/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace DealershipManager.Models
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsCompany { get; set; }
+    }
+}
